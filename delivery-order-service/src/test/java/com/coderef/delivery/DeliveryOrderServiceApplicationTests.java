a5f2261045aeab5da9b03d9b37e263087457318f
Jardel Kuhnen <jardel.kuhnen@hbsis.com.br>
2020-05-25 22:11:33 -0300

    Commit the final template

---
+++ delivery-order-service/src/test/java/com/coderef/delivery/DeliveryOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderef.delivery;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
